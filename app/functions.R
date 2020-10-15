4a0c5f0fb742abcfb96d60012b90e646306d22f5
frekkanzer2 <abatefrancesco98@gmail.com>
2020-10-15 18:52:27 +0200

    Added more graphical functions + added one dependency

--- app/functions.R
+++ app/functions.R
@@ -1,21 +1,84 @@
-# This function will create a Pareto diagram based on the array parameter
-createPareto <- function (array_to_analyze){
-  # Sum all items
-  mySum = sum(array_to_analyze)
-  array_to_analyze <- array_to_analyze / mySum
+# createPareto : displays a Pareto diagram with frequences
+##  input -> name : gives a name to the pareto
+##           arrayToAnalyze : array with indexes to display
+createPareto <- function (name, arrayToAnalyze) {
+  
+  # Calculating frequences
+  mySum = sum(arrayToAnalyze)
+  arrayToAnalyze <- arrayToAnalyze / mySum
   
   # Ordering 
-  ari_ordered <- order(array_to_analyze, decreasing = TRUE)
+  ari_ordered <- order(arrayToAnalyze, decreasing = TRUE)
   
   # Creating graphic
-  bp <- barplot(array_to_analyze[ari_ordered],
-                col=rainbow(length(array_to_analyze)), 
+  bp <- barplot(arrayToAnalyze[ari_ordered], main = name,
+                col=rainbow(length(arrayToAnalyze)), 
                 names = mydf$Regioni[ari_ordered], las=2, ylim = c(0, 1.05))
   
-  lines(bp, cumsum(array_to_analyze[ari_ordered]), 
+  lines(bp, cumsum(arrayToAnalyze[ari_ordered]), 
         type = "b", pch = 16)
   
-  text(bp - 0.2, cumsum(array_to_analyze[ari_ordered]) + 0.03,
-       paste(format(cumsum(array_to_analyze[ari_ordered]) * 100,
+  text(bp - 0.2, cumsum(arrayToAnalyze[ari_ordered]) + 0.03,
+       paste(format(cumsum(arrayToAnalyze[ari_ordered]) * 100,
                     digits = 2), "%"))
+}
+
+# createBarPlot_base : displays a BarPlot diagram
+##  input -> name : gives a name to the pareto
+##           arrayToAnalyze : array with indexes to display
+##           frequency : 0 -> use indexes | 1 -> use frequences
+createBarPlot_base <- function (name, arrayToAnalyze, frequency) {
+  if (frequency == 1) {
+    mySum = sum(arrayToAnalyze)
+    arrayToAnalyze <- arrayToAnalyze / mySum
+  }
+  barplot(arrayToAnalyze, col=rainbow(length(arrayToAnalyze)),
+          names = mydf$Regioni, las=2, main = name)
+}
+
+# createBarPlot_limit : displays a BarPlot diagram with a max limit on y axis
+##  input -> name : gives a name to the pareto
+##           arrayToAnalyze : array with indexes to display
+##           maxLimit : the y limit on the axis
+##           frequency : 0 -> use indexes | 1 -> use frequences
+createBarPlot_limit <- function (name, arrayToAnalyze, maxLimit, frequency) {
+  if (frequency == 1) {
+    mySum = sum(arrayToAnalyze)
+    arrayToAnalyze <- arrayToAnalyze / mySum
+  }
+  barplot(arrayToAnalyze, col=rainbow(length(arrayToAnalyze)),
+          ylim = c(0, maxLimit), names = mydf$Regioni, las=2, main = name)
+}
+
+# createBarPlot_ordered : displays a BarPlot diagram with a max limit on y axis
+#                         and with ordered indexes
+##  input -> name : gives a name to the pareto
+##           arrayToAnalyze : array with indexes to display
+##           maxLimit : the y limit on the axis
+##           frequency : 0 -> use indexes | 1 -> use frequences
+createBarPlot_ordered <- function (name, arrayToAnalyze, maxLimit, frequency) {
+  if (frequency == 1) {
+    mySum = sum(arrayToAnalyze)
+    arrayToAnalyze <- arrayToAnalyze / mySum
+  }
+  data_order <- order(arrayToAnalyze, decreasing = TRUE)
+  barplot(arrayToAnalyze[data_order],
+          col=rainbow(length(arrayToAnalyze)),
+          ylim = c(0, maxLimit), main = name,
+          names = mydf$Regioni[data_order], las=2)
+}
+
+# createBoxPlot_base : displays a BoxPlot with default settings
+##  input -> name : gives a name to the pareto
+##           arrayToAnalyze : array with indexes to display
+createBoxPlot_base <- function(name, arrayToAnalyze) {
+  boxplot(arrayToAnalyze, col="green", main = name)
+}
+
+# createBoxPlot_colored : displays a BoxPlot with custom color
+##  input -> name : gives a name to the pareto
+##           arrayToAnalyze : array with indexes to display
+##           customColor : color of the BoxPlot
+createBoxPlot_colored <- function(name, arrayToAnalyze, customColor) {
+  boxplot(arrayToAnalyze, col=customColor, main = name)
 }
--- app/main.R
+++ app/main.R
@@ -1,17 +1,22 @@
 mydf <- Dati
 mydf
-for (i in 1:28){
+for (i in 1:20){
   for (j in 2:7) {
     mydf[i, j] <- as.integer(mydf[i, j])
   }
 }
+remove(i, j)
 
 
 main <- function() {
   
-  # Getting the row array
+  # Getting the row arrays
   arrayRaccoltaIndifferenziata = mydf$`Raccolta indifferenziata`
-  createPareto(arrayRaccoltaIndifferenziata)
+  arrayCarta = mydf$`Carta e cartone`
+  arrayPlastica = mydf$Plastica
+  arrayVetro = mydf$Vetro
+  arrayUmido = mydf$`Rifiuti organici`
+  arrayAltro = mydf$Altro
   
 }
 
