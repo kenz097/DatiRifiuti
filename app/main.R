aa8709d16c8b40e3722ad02080aaf4c06eb8d122
frekkanzer2 <abatefrancesco98@gmail.com>
2020-10-17 17:21:13 +0200

    Barplot with freqs + fix barplots

--- app/main.R
+++ app/main.R
@@ -1,7 +1,10 @@
 mydf <- Dati
+df <- data.frame(X1 = rep(0), X2 = rep(0), X3 = rep(0),
+                 X4 = (0), X5 = rep(0), X6 = rep(0))
 for (i in 1:20){
   for (j in 2:7) {
     mydf[i, j] <- as.integer(mydf[i, j])
+    df[i, j-1] <- mydf[i, j]
   }
 }
 remove(i, j)
@@ -9,6 +12,8 @@ remove(i, j)
   ### MAIN ###
   
   # Getting the row arrays
+  labelsRifiuti <- c("Raccolta Indifferenziata","Rifiuti organici",
+                     "Carta e cartone","Vetro","Plastica","Altro")
   datiRaccoltaIndifferenziata = mydf$`Raccolta indifferenziata`
   datiCarta = mydf$`Carta e cartone`
   datiPlastica = mydf$Plastica
@@ -19,11 +24,21 @@ remove(i, j)
   # ATTENTION
     # Before continuing, re-group all graphs in only one environment
   
-  createBarPlot_limit("Carta e Cartone", datiCarta, 1000000)
-  createBarPlot_limit("Plastica", datiPlastica, 1000000)
-  createBarPlot_limit("Vetro", datiVetro, 1000000)
-  createBarPlot_limit("Carta e Cartone", datiCarta, 1000000)
-  createBarPlot_limit("Carta e Cartone", datiCarta, 1000000)
+  createBarPlot_limit("Carta e Cartone", datiCarta, 1.05, 1)
+  createBarPlot_limit("Plastica", datiPlastica, 1.05, 1)
+  createBarPlot_limit("Vetro", datiVetro, 1.05, 1)
+  createBarPlot_limit("Organici", datiUmido, 1.05, 1)
+  createBarPlot_limit("Altro", datiAltro, 1.05, 1)
+  createBarPlot_limit("Rifiuti Indifferenziata", 
+                      datiRaccoltaIndifferenziata, 1.05, 1)
   
   # OR
     # Jump chapter 2 and start another chapter by ignoring previous code
+  
+  #barplot
+  barplot(t(df), main="Frequenza assoluta congiunta",
+          legend=labelsRifiuti,
+          col=rainbow(6))
+  
+  
+  
