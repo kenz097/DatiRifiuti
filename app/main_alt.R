ae1fad3f2d80e9a6650f0222ab6c096bcf95ed82
frekkanzer2 <abatefrancesco98@gmail.com>
2020-10-23 18:42:21 +0200

    Chap 5

--- app/main_alt.R
+++ app/main_alt.R
@@ -19,4 +19,75 @@ remove(i, j)
   waste = SicilyValue - mean(datiRaccoltaIndifferenziata)
   waste # result is 1143139
   
+  umido = mydf$`Rifiuti organici`
+  cartone = mydf$`Carta e cartone`
+  umido = umido / 1000
+  cartone = cartone / 1000
+  
+  plot(cartone, umido, main = "Rifiuti organici in funzione del cartone",
+      xlab = "Cartone", ylab = "Organico", col = "red")
+  abline(v=median(cartone), lty=1, col="magenta")
+  abline(v=mean(cartone), lty=2, col="blue")
+  abline(h=median(umido), lty=1, col="magenta")
+  abline(h=mean(umido), lty=2, col="blue")
+  legend (50, 1200, c("Mediana", "Media"), pch =0, col=c("magenta", "blue"),
+          cex =0.8)
+  
+  median(cartone)
+  mean(cartone)
+  median(umido)
+  mean(umido)
+  
+  cov(cartone, umido)
+  cor(cartone, umido)
+  
+  plot(cartone, umido, main = "Rifiuti organici in funzione del cartone",
+       xlab = "Cartone", ylab = "Organico", col = "red")
+  abline(v=median(cartone), lty=1, col="magenta")
+  abline(v=mean(cartone), lty=2, col="blue")
+  abline(h=median(umido), lty=1, col="magenta")
+  abline(h=mean(umido), lty=2, col="blue")
+  legend (50, 1200, c("Mediana", "Media"), pch =0, col=c("magenta", "blue"),
+          cex =0.8)
+  abline(lm(umido~cartone), col="blue")
+  
+  beta <- (sd(umido) / sd(cartone)) * cor(cartone, umido)
+  alpha <- mean(umido) - beta*mean(cartone)
+  c(alpha, beta)
+  
+  
+  plot(cartone, umido, main = "Rifiuti organici in funzione del cartone",
+       xlab = "Cartone", ylab = "Organico", col = "red")
+  abline(lm(umido~cartone), col="blue")
+  stime<-fitted(lm(umido~cartone))
+  segments(cartone, stime, cartone, umido, col="magenta")
+  
+  residui <- resid(lm(umido~cartone))
+  plot(cartone, residui, main = "Diagramma dei residui",
+       xlab = "Cartone", ylab = "Residui", col = "red", pch=9)
+  abline(h=0, col="blue", lty=2)
+  
+  summary (lm(umido~cartone))$r.square
+  
+  toAnalyze = data.frame(mydf$`Rifiuti organici`, mydf$`Carta e cartone`,
+                         mydf$Vetro, mydf$Plastica, mydf$Altro)
+  toAnalyze = toAnalyze / 1000
+  pairs(toAnalyze)
+  
+  cov(toAnalyze)
+  cor(toAnalyze)
+  
+  lm(toAnalyze$mydf..Rifiuti.organici.~toAnalyze$mydf..Carta.e.cartone.+
+       toAnalyze$mydf.Vetro+toAnalyze$mydf.Plastica+toAnalyze$mydf.Altro)
+  
+  stime = fitted(lm(toAnalyze$mydf..Rifiuti.organici.~
+                      toAnalyze$mydf..Carta.e.cartone.+toAnalyze$mydf.Vetro+
+                      toAnalyze$mydf.Plastica+toAnalyze$mydf.Altro))
+  residui = resid(lm(toAnalyze$mydf..Rifiuti.organici.~
+                       toAnalyze$mydf..Carta.e.cartone.+toAnalyze$mydf.Vetro+
+                       toAnalyze$mydf.Plastica+toAnalyze$mydf.Altro))
+  stime
+  residui
+  
+  
   
